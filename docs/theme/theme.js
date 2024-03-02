3f88ecc080e8a8e5e8bf7751584351a03c2bc859
Alan Smith <commit@alanwsmith.com>
2024-03-01 21:48:55 -0500

    Basic menu open and close in place

--- docs/theme/theme.js
+++ docs/theme/theme.js
@@ -1,16 +1,34 @@
-function toggle_menu_folder_visibility(event) {
-  console.log(event);
+function toggle_folder_menu_status(event) {
+  console.log("asdf")
+  let ds = event.target.dataset
+  const child_menu_items = document.getElementById(`folder_menu_children_${ds.page_id}`)
+  if (ds.status == "open") {
+    event.target.classList.remove("folder_menu_opened")
+    event.target.classList.add("folder_menu_closed")
+    child_menu_items.classList.remove("folder_menu_children_open")
+    child_menu_items.classList.add("folder_menu_children_closed")
+    ds.status = "closed"
+  } else if (ds.status == "closed") {
+    event.target.classList.add("folder_menu_opened")
+    event.target.classList.remove("folder_menu_closed")
+    child_menu_items.classList.remove("folder_menu_children_closed")
+    child_menu_items.classList.add("folder_menu_children_open")
+    ds.status = "open"
+  } 
 }
 
-function add_menu_folder_button_listeners() {
-  const buttons = document.getElementsByClassName("menu_folder_button")
+function add_folder_menu_button_listeners() {
+  console.log("---")
+  const buttons = document.getElementsByClassName("folder_menu_button")
   for (let button of buttons) {
-    button.addEventListener("click", toggle_menu_folder_visibility)
+  console.log("a")
+    button.addEventListener("click", toggle_folder_menu_status)
   }
 }
 
 
+
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Content loaded")
-  add_menu_folder_button_listeners() 
+  add_folder_menu_button_listeners() 
 })
