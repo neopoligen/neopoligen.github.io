745f48db9b9f1d3aa81ba9cc03039f3107d94364
Alan Smith <commit@alanwsmith.com>
2024-03-02 13:08:18 -0500

    Getting ready to move to nav tree approach

--- docs/theme/theme.js
+++ docs/theme/theme.js
@@ -1,18 +1,22 @@
 function toggle_folder_menu_status(event) {
-  console.log("asdf")
   let ds = event.target.dataset
   const child_menu_items = document.getElementById(`folder_menu_children_${ds.page_id}`)
+  const li_item = document.getElementById(`folder_menu_item_${ds.page_id}`)
   if (ds.status == "open") {
     event.target.classList.remove("folder_menu_opened")
     event.target.classList.add("folder_menu_closed")
     child_menu_items.classList.remove("folder_menu_children_open")
     child_menu_items.classList.add("folder_menu_children_closed")
+    li_item.classList.remove("folder_menu_opened_directory_item")
+    li_item.classList.add("folder_menu_closed_directory_item")
     ds.status = "closed"
   } else if (ds.status == "closed") {
     event.target.classList.add("folder_menu_opened")
     event.target.classList.remove("folder_menu_closed")
     child_menu_items.classList.remove("folder_menu_children_closed")
     child_menu_items.classList.add("folder_menu_children_open")
+    li_item.classList.add("folder_menu_opened_directory_item")
+    li_item.classList.remove("folder_menu_closed_directory_item")
     ds.status = "open"
   } 
 }
