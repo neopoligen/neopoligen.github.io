a7a8f44f46f8258b7ce71f8056515a46e52dbd4d
Alan Smith <commit@alanwsmith.com>
2024-03-04 22:10:14 -0500

    Added TitleFolderActive

--- docs/theme/theme.js
+++ docs/theme/theme.js
@@ -8,34 +8,11 @@ function toggle_nav_menu_item(event) {
     li.classList.add("nav_menu_opened_title_folder")
     ds.status = "open"
   } else {
-    console.log(".")
+    li.classList.remove("nav_menu_active_title_folder")
     li.classList.remove("nav_menu_opened_title_folder")
     li.classList.add("nav_menu_closed_title_folder")
     ds.status = "closed"
   }
-
-
-
-  // const child_menu_items = document.getElementById(`folder_menu_children_${ds.page_id}`)
-  // const li_item = document.getElementById(`folder_menu_item_${ds.page_id}`)
-  // if (ds.status == "open") {
-  //   event.target.classList.remove("folder_menu_opened")
-  //   event.target.classList.add("folder_menu_closed")
-  //   child_menu_items.classList.remove("folder_menu_children_open")
-  //   child_menu_items.classList.add("folder_menu_children_closed")
-  //   li_item.classList.remove("folder_menu_opened_directory_item")
-  //   li_item.classList.add("folder_menu_closed_directory_item")
-  //   ds.status = "closed"
-  // } else if (ds.status == "closed") {
-  //   event.target.classList.add("folder_menu_opened")
-  //   event.target.classList.remove("folder_menu_closed")
-  //   child_menu_items.classList.remove("folder_menu_children_closed")
-  //   child_menu_items.classList.add("folder_menu_children_open")
-  //   li_item.classList.add("folder_menu_opened_directory_item")
-  //   li_item.classList.remove("folder_menu_closed_directory_item")
-  //   ds.status = "open"
-  // } 
-
 }
 
 function add_folder_menu_button_listeners() {
@@ -44,7 +21,6 @@ function add_folder_menu_button_listeners() {
     button.addEventListener("click", toggle_nav_menu_item)
   }
 }
-
 
 
 document.addEventListener("DOMContentLoaded", () => {
