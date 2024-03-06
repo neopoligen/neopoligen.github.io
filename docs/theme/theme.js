244d17b1edcb684e517fc031b42b14c8ee5accaa
Alan Smith <commit@alanwsmith.com>
2024-03-05 20:22:20 -0500

    Finished initial collection menu implementation

--- docs/theme/theme.js
+++ docs/theme/theme.js
@@ -1,29 +1,29 @@
-function toggle_nav_menu_item(event) {
+function toggle_collection_menu_item(event) {
   console.log("got click")
   let el = event.target
   let ds = el.dataset
-  let li = document.getElementById(`nav_menu_item_${ds.page_id}`)
+  let li = document.getElementById(`${ds.menu}_${ds.id}`)
    if (ds.status == "closed") {
-    li.classList.remove("nav_menu_closed_title_folder")
-    li.classList.add("nav_menu_opened_title_folder")
+    li.classList.remove("title_folder_closed")
+    li.classList.add("title_folder_opened")
     ds.status = "open"
   } else {
-    li.classList.remove("nav_menu_active_title_folder")
-    li.classList.remove("nav_menu_opened_title_folder")
-    li.classList.add("nav_menu_closed_title_folder")
+    li.classList.remove("title_folder_active")
+    li.classList.remove("title_folder_opened")
+    li.classList.add("title_folder_closed")
     ds.status = "closed"
   }
 }
 
-function add_folder_menu_button_listeners() {
-  const buttons = document.getElementsByClassName("nav_menu_button")
+function add_collection_menu_button_listeners() {
+  const buttons = document.getElementsByClassName("collection_menu_button")
   for (let button of buttons) {
-    button.addEventListener("click", toggle_nav_menu_item)
+    button.addEventListener("click", toggle_collection_menu_item)
   }
 }
 
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Content loaded")
-  add_folder_menu_button_listeners() 
+  add_collection_menu_button_listeners() 
 })
