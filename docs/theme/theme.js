414f3930d3f9ba2dc4a4dca81caaafeda5d537bf
Alan Smith <commit@alanwsmith.com>
2024-03-06 13:05:11 -0500

    Add show/hide original content

--- docs/theme/theme.js
+++ docs/theme/theme.js
@@ -1,9 +1,22 @@
+function add_collection_menu_button_listeners() {
+  const buttons = document.getElementsByClassName("collection_menu_button")
+  for (let button of buttons) {
+    button.addEventListener("click", toggle_collection_menu_item)
+  }
+}
+
+function add_original_content_toggle_listeners() {
+  const buttons = document.getElementsByClassName("original_content_toggle")
+  for (let button of buttons) {
+    button.addEventListener("click", toggle_original_content)
+  }
+}
+
 function toggle_collection_menu_item(event) {
-  console.log("got click")
   let el = event.target
   let ds = el.dataset
   let li = document.getElementById(`${ds.menu}_${ds.id}`)
-   if (ds.status == "closed") {
+  if (ds.status == "closed") {
     li.classList.remove("title_folder_closed")
     li.classList.add("title_folder_opened")
     ds.status = "open"
@@ -15,15 +28,29 @@ function toggle_collection_menu_item(event) {
   }
 }
 
-function add_collection_menu_button_listeners() {
-  const buttons = document.getElementsByClassName("collection_menu_button")
-  for (let button of buttons) {
-    button.addEventListener("click", toggle_collection_menu_item)
+function toggle_original_content(event) {
+  let inner_el = document.getElementById(`inner_original_content_toggle`)
+  let inner_ds = inner_el.dataset
+  let outer_el = document.getElementById(`outer_original_content_toggle`)
+  let outer_ds = outer_el.dataset
+  let display = document.getElementById(`original_content_display`)
+  if (event.target.dataset.status == "hidden") {
+    display.classList.remove("hidden")
+    inner_ds.status = "visible"
+    outer_ds.status = "visible"
+    inner_el.innerHTML = "Hide original content file"
+    outer_el.innerHTML = "Hide original content file"
+  } else {
+    display.classList.add("hidden")
+    inner_ds.status = "hidden"
+    outer_ds.status = "hidden"
+    inner_el.innerHTML = "Show original content file"
+    outer_el.innerHTML = "Show original content file"
   }
 }
 
-
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Content loaded")
-  add_collection_menu_button_listeners() 
+  add_collection_menu_button_listeners()
+  add_original_content_toggle_listeners()
 })
