67f347c71ab11fc8d09cceee3e589ca70da9fd32
Alan Smith <commit@alanwsmith.com>
2024-04-02 13:39:12 -0400

    Put nav button functionality back in place

--- themes/neopoligen.com/_assets/theme.js
+++ themes/neopoligen.com/_assets/theme.js
@@ -1,9 +1,9 @@
-function add_selector_toggle_functions(selector, on_function, off_function) {
+function add_selector_event_toggle_functions(selector, event_type, on_function, off_function) {
   console.log(`- Adding toggle functions to: ${selector}`)
   let elements = document.querySelectorAll(selector)
   elements.forEach((element) => {
     console.log(`- Updating element`)
-    element.addEventListener("click", (event) => { 
+    element.addEventListener(event_type, (event) => { 
         elements.forEach((update_element) => {
             update_element.classList.toggle("on")
             update_element.classList.toggle("off")
@@ -22,27 +22,62 @@ function add_selector_toggle_functions(selector, on_function, off_function) {
 }
 
 
-function add_selector_listener_function(selector, listener, func) {
-  console.log(`- Adding '${listener}' listener to: ${selector}`)
+function add_selector_listener_function(selector, event_type, trigger_function) {
+  console.log(`- Adding '${event_type}' listener to: ${selector}`)
   let elements = document.querySelectorAll(selector)
   elements.forEach((element) => {
-    element.addEventListener(listener, func)
+    element.addEventListener(event_type, trigger_function)
   })
 }
 
-// function add_nav_menu_button_listeners() {
-//   const buttons = document.getElementsByClassName("nav_menu_button")
-//   for (let button of buttons) {
-//     button.addEventListener("click", toggle_nav_menu_item)
-//   }
-// }
 
-// function add_original_content_toggle_listeners() {
-//   const buttons = document.getElementsByClassName("original_content_toggle")
-//   for (let button of buttons) {
-//     button.addEventListener("click", toggle_original_content)
-//   }
-// }
+function hide_original_source_content(event) {
+  let display = document.getElementById(`original_content_display`)
+  display.classList.add("hidden")
+}
+
+
+function show_original_source_content(event) {
+  let display = document.getElementById(`original_content_display`)
+  display.classList.remove("hidden")
+}
+
+function toggle_nav_menu_item(event) {
+  console.log("hit toggle_nav_menu_item")
+  let el = event.target
+  let ds = el.dataset
+  let li = document.getElementById(`${ds.menu}_${ds.id}`)
+  if (ds.status == "closed") {
+    li.classList.remove("title_folder_closed")
+    li.classList.add("title_folder_opened")
+    ds.status = "open"
+  } else {
+    li.classList.remove("title_folder_active")
+    li.classList.remove("title_folder_opened")
+    li.classList.add("title_folder_closed")
+    ds.status = "closed"
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  add_selector_event_toggle_functions(
+    ".original_content_toggle", 
+    "click",
+    show_original_source_content, 
+    hide_original_source_content
+  )
+  add_selector_listener_function(
+    ".nav_menu_button",
+    "click",
+    toggle_nav_menu_item
+  )
+})
+
+
+
+
+
+
 
 // function add_style_to_selector(style, selector) {
 //   let elements = document.querySelectorAll(selector)
@@ -58,57 +93,10 @@ function add_selector_listener_function(selector, listener, func) {
 //   })
 // }
 
-// function toggle_nav_menu_item(event) {
-//   console.log("hit toggle_nav_menu_item")
-//   let el = event.target
-//   let ds = el.dataset
-//   let li = document.getElementById(`${ds.menu}_${ds.id}`)
-//   if (ds.status == "closed") {
-//     li.classList.remove("title_folder_closed")
-//     li.classList.add("title_folder_opened")
-//     ds.status = "open"
-//   } else {
-//     li.classList.remove("title_folder_active")
-//     li.classList.remove("title_folder_opened")
-//     li.classList.add("title_folder_closed")
-//     ds.status = "closed"
-//   }
-// }
-
-
-function show_original_source_content(event) {
-  let display = document.getElementById(`original_content_display`)
-  display.classList.remove("hidden")
-}
-
-
-function hide_original_source_content(event) {
-  let display = document.getElementById(`original_content_display`)
-  display.classList.add("hidden")
-}
 
 
 
-// function toggle_original_content(event) {
-//   let inner_el = document.getElementById(`inner_original_content_toggle`)
-//   let inner_ds = inner_el.dataset
-//   let outer_el = document.getElementById(`outer_original_content_toggle`)
-//   let outer_ds = outer_el.dataset
-//   let display = document.getElementById(`original_content_display`)
-//   if (event.target.dataset.status == "hidden") {
-//     display.classList.remove("hidden")
-//     inner_ds.status = "visible"
-//     outer_ds.status = "visible"
-//     inner_el.innerHTML = "Hide original content file"
-//     outer_el.innerHTML = "Hide original content file"
-//   } else {
-//     display.classList.add("hidden")
-//     inner_ds.status = "hidden"
-//     outer_ds.status = "hidden"
-//     inner_el.innerHTML = "Show original content file"
-//     outer_el.innerHTML = "Show original content file"
-//   }
-// }
+
 
 // function toggle_style_via_selector(style, selector) {
 //   let elements = document.querySelectorAll(selector)
@@ -137,21 +125,45 @@ function hide_original_source_content(event) {
 
 
 
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Content loaded")
-  
-  add_selector_toggle_functions(
-    ".original_content_toggle", show_original_source_content, hide_original_source_content
-  )
-  // add_listener_function_to_selector(
-  //   "click", toggle_nav_menu_item, ".nav_menu_button"
-  // )
-  // add_listener_function_to_selector(
-  //   "click", toggle_button, { 
-  //     selector: ".original_content_toggle" 
-  //   }
-  // )
 
 
-})
+
+//////////////////////////////////
+
+// deprecated
+
+// function add_nav_menu_button_listeners() {
+//   const buttons = document.getElementsByClassName("nav_menu_button")
+//   for (let button of buttons) {
+//     button.addEventListener("click", toggle_nav_menu_item)
+//   }
+// }
+
+// function add_original_content_toggle_listeners() {
+//   const buttons = document.getElementsByClassName("original_content_toggle")
+//   for (let button of buttons) {
+//     button.addEventListener("click", toggle_original_content)
+//   }
+// }
+
+// function toggle_original_content(event) {
+//   let inner_el = document.getElementById(`inner_original_content_toggle`)
+//   let inner_ds = inner_el.dataset
+//   let outer_el = document.getElementById(`outer_original_content_toggle`)
+//   let outer_ds = outer_el.dataset
+//   let display = document.getElementById(`original_content_display`)
+//   if (event.target.dataset.status == "hidden") {
+//     display.classList.remove("hidden")
+//     inner_ds.status = "visible"
+//     outer_ds.status = "visible"
+//     inner_el.innerHTML = "Hide original content file"
+//     outer_el.innerHTML = "Hide original content file"
+//   } else {
+//     display.classList.add("hidden")
+//     inner_ds.status = "hidden"
+//     outer_ds.status = "hidden"
+//     inner_el.innerHTML = "Show original content file"
+//     outer_el.innerHTML = "Show original content file"
+//   }
+// }
 
